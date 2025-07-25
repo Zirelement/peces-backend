0f21e95061aa401c721bb87e853469f51799c907
Ciro <zirelement44@gmail.com>
2025-07-24 22:26:23 -0500

    Añadir modelos de Mongoose para Species y User

---
+++ models/Species.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const speciesSchema = new mongoose.Schema({
+  nombre_comun:        { type: String, required: true },
+  nombre_cientifico:   { type: String, required: true },
+  familia:             { type: String, required: true },
+  alimentacion:        { type: String },
+  estado_conservacion: { type: String },
+  imagen_url:          { type: String },
+  enabled:             { type: Boolean, default: true }
+});
+
+module.exports = mongoose.model('Species', speciesSchema);
